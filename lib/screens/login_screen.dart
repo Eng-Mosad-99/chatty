6760f13cd6dfc1d9b38909bef1bbbbc5bf633904
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-11-11 20:34:30 +0200

    creating buttons

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -55,65 +55,65 @@ class _LoginScreenState extends State<LoginScreen> {
                     const SizedBox(
                       height: 16,
                     ),
-                    // Row(
-                    //   children: [
-                    //     const Spacer(),
-                    //     GestureDetector(
-                    //       child: const Text("Forget Password?"),
-                    //       onTap: () {
-                    //         // Navigator.push(
-                    //         //     context,
-                    //         //     MaterialPageRoute(
-                    //         //       builder: (context) => ForgetScreen(),
-                    //         //     ));
-                    //       },
-                    //     )
-                    //   ],
-                    // ),
-                    // const SizedBox(
-                    //   height: 16,
-                    // ),
-                    // ElevatedButton(
-                    //   onPressed: () {},
-                    //   style: ElevatedButton.styleFrom(
-                    //     shape: RoundedRectangleBorder(
-                    //         borderRadius: BorderRadius.circular(12)),
-                    //     // backgroundColor: kPrimaryColor,
-                    //     padding: const EdgeInsets.all(16),
-                    //   ),
-                    //   child: Center(
-                    //     child: Text(
-                    //       "Login".toUpperCase(),
-                    //       style: const TextStyle(color: Colors.black),
-                    //     ),
-                    //   ),
-                    // ),
-                    // const SizedBox(
-                    //   height: 16,
-                    // ),
-                    // OutlinedButton(
-                    //   style: OutlinedButton.styleFrom(
-                    //     shape: RoundedRectangleBorder(
-                    //         borderRadius: BorderRadius.circular(12)),
-                    //     padding: const EdgeInsets.all(16),
-                    //   ),
-                    //   onPressed: () {
-                    //     // Navigator.pushAndRemoveUntil(
-                    //     //     context,
-                    //     //     MaterialPageRoute(
-                    //     //       builder: (context) => SetupProfile(),
-                    //     //     ),
-                    //     //     (route) => false);
-                    //   },
-                    //   child: Center(
-                    //     child: Text(
-                    //       "Create Account".toUpperCase(),
-                    //       style: TextStyle(
-                    //         color: Theme.of(context).colorScheme.onBackground,
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        GestureDetector(
+                          child: const Text("Forget Password?"),
+                          onTap: () {
+                            // Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //       builder: (context) => ForgetScreen(),
+                            //     ));
+                          },
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        // backgroundColor: kPrimaryColor,
+                        padding: const EdgeInsets.all(16),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login".toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        padding: const EdgeInsets.all(16),
+                      ),
+                      onPressed: () {
+                        // Navigator.pushAndRemoveUntil(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //       builder: (context) => SetupProfile(),
+                        //     ),
+                        //     (route) => false);
+                      },
+                      child: Center(
+                        child: Text(
+                          "Create Account".toUpperCase(),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                        ),
+                      ),
+                    ),
                   
                   ],
                 ),
